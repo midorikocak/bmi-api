16793c34eef130277072b07e28bc78cb40844d5f
margaobradora <marmodovar@gmail.com>
2022-01-10 21:29:40 +0100

    changes

---
+++ frontend/js/main.js
@@ -0,0 +1,24 @@
+// weight
+// height
+// result
+// calculate
+
+let weightInput = document.getElementById("weight");
+let heightInput = document.getElementById("height");
+let resultsArea = document.getElementById("result");
+let resultButton = document.getElementById("calculate");
+
+// formula BMI (function)
+
+function calculateBMI(height, weight) {
+  return (weight / height / height) * 10000;
+}
+//
+
+// event listeners
+
+resultButton.addEventListener("click", function () {
+  let weight = weightInput.value;
+  let height = heightInput.value;
+  resultsArea.innerHTML = calculateBMI(weight, height);
+});
