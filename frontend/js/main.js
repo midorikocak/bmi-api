ed088a8f990fcfaceb0c44d70115cbc51ee4eb11
catabuades <catabuades@gmail.com>
2022-01-10 21:29:43 +0100

    Add html structure, css styles and some js

---
+++ frontend/js/main.js
@@ -0,0 +1,20 @@
+// CATA BUADES
+
+let weightInput = document.getElementById('Weight');
+let heightInput = document.getElementById('Height');
+let sendButton = document.getElementById('Send');
+let resultText = document.getElementById('Result');
+
+function bmiCalculation(weight, height) {
+
+  resultText.innerHTML = (weight / height / height) * 10000;
+  return;
+}
+
+sendButton.addEventListener('click', function() {
+  bmiCalculation(weightInput.value, heightInput.value);
+});
+
+//bmiCalculation(weightInput, heightInput);
+
+// [weight (kg) / height (cm) / height (cm)] x 10,000
