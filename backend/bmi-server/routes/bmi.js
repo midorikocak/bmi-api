4ebc3c1938901f042da68692e578a90dd9fb9239
anionce <anionce91@gmail.com>
2022-01-12 22:45:44 +0100

    added route

---
+++ backend/bmi-server/routes/bmi.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+
+    let weight = req.body.weight;
+    let height = req.body.height;
+
+    let bmi = ((weight / height / height) * 10000).toFixed(2)
+    let dateTime = (new Date()).toLocaleString();
+
+    let responseObject = {bmi: bmi, dateTime: dateTime}
+    res.json(responseObject);
+});
+
+module.exports = router;
