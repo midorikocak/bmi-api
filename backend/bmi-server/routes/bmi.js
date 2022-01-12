bfb0dca05714362e16ea5374609ad184036a0c3d
marta <vilaseca.marta@gmail.com>
2022-01-12 21:52:37 +0100

    Added package-lock.json and the bmi routes

---
+++ backend/bmi-server/routes/bmi.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* POST BMI page */
+router.post('/', function(req, res, next) {
+    console.log(req.body)
+
+    let weight = req.body.weight;
+    let height = req.body.height;
+
+    let bmi = ((weight / height / height) * 10000).toFixed(2);
+    let dateTime = (new Date()).toLocaleString();
+    
+    
+    let responseObject = {bmi : bmi, dateTime: dateTime};
+
+    res.json(responseObject);
+});
+
+module.exports = router;
