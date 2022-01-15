e24a967385361a02dba60cd8e3cabeb2d1fd7d45
margaobradora <marmodovar@gmail.com>
2022-01-15 13:11:24 +0100

    ultimos cambios

---
+++ backend/bmi-server/models/measurements.js
@@ -0,0 +1,33 @@
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("./data/database.db");
+
+/* run: is the method when we dont have a data response*/
+/* all: gives me all records from the query*/
+/* get: gives me only one records from the query*/
+function add(data, callback) {
+  const sql =
+    "INSERT INTO measurements (bmi, datetime) VALUES ('24,5', '15/01/2022, 11:34:41')";
+  db.run();
+}
+
+function all(callback) {
+  const sql = "SELECT * FROM measurements";
+  db.all();
+}
+
+function one(id, callback) {
+  const sql = "SELECT * FROM measurements id=1";
+  db.get();
+}
+
+function update(id, newData, callback) {
+  const sql = "UPDATE measurements SET bmi = 24.6 WHERE id = 1";
+  db.run();
+}
+
+function remove(id, callback) {
+  const sql = "DELETE FROM measurements WHERE id=1";
+  db.run();
+}
+
+module.exports = { add, all, one, update, remove };
---
+++ backend/bmi-server/routes/history.js
@@ -0,0 +1,83 @@
+var express = require("express");
+const { response } = require("../app");
+var router = express.Router();
+var Measurements = require("../models/measurements");
+
+/*CREATE  MARGAAA*/
+router.post("/", function (req, res, next) {
+  console.log(req.body);
+
+  let weight = req.body.weight;
+  let height = req.body.height;
+  let bmi = ((weight / height / height) * 10000).toFixed(2);
+  let dateTime = new Date().toLocaleString();
+  console.log(dateTime);
+
+  let responseObject = { bmi: bmi, dateTime: dateTime };
+  Measurements.add(responseObject, function (err, data) {
+    if (err) {
+      res.status(500).send("Server error");
+    } else {
+      res.json(responseObject);
+    }
+  });
+
+  res.json(responseObject);
+});
+
+/*ALL (read) MARGAAA*/
+router.get("/", function (req, res, next) {
+  Measurements.all(function (err, data) {
+    if (err) {
+      res.status(500).send("Server error");
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+/*ONE (read) MARGAAA*/
+router.get("/:id", function (req, res, next) {
+  const id = parseInt(req.params.id);
+  Measurements.one(id, function (err, data) {
+    if (err) {
+      res.status(500).send("There was an error");
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+/*UPDATE MARGAAA*/
+router.put("/:id", function (req, res, next) {
+  const id = parseInt(req.params.id);
+
+  let weight = req.body.weight;
+  let height = req.body.height;
+
+  let bmi = ((weight / height / height) * 10000).toFixed(2);
+  let dateTime = new Date().toLocaleString();
+
+  let newData = { bmi: bmi, dateTime: dateTime };
+
+  Measurements.update(id, newData, function (err) {
+    if (err) {
+      res.status(500).send("Server error");
+    } else {
+      res.status(200).send();
+    }
+  });
+});
+
+/*DELETE MARGAAA*/
+router.delete("/:id", function (req, res, next) {
+  Measurements.remove(id, function (err) {
+    if (err) {
+      res.status(500).send("Server error");
+    } else {
+      res.status(200).send();
+    }
+  });
+});
+
+module.exports = router;
