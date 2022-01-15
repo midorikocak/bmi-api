3af9cb6a49822a8e448dc5214fb1d7e7d50f5197
marta <vilaseca.marta@gmail.com>
2022-01-15 13:11:26 +0100

    adding database and models

---
+++ backend/bmi-server/models/measurements.js
@@ -0,0 +1,30 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./data/database.db');
+
+function add(data, callback) {
+    const sql = "INSERT INTO measurements (bmi, datetime) VALUES ('24,5', '15/01/2022, 11:34:41')";
+    db.run();
+}
+
+/* get gives only one record from query, all gives all of them */
+function all(callback) {
+    const sql = "SELECT * FROM measurements";
+    db.all();
+}
+
+function one(id, callback) {
+    const sql = "SELECT*FROM measurements WHERE id=?";
+    db.get();
+}
+
+function update(id, newData, callback) {
+    const sql = "UPDATE measurements SET bmi = 24.6 WHERE id = 1";
+    db.run();
+}
+
+function remove(id, callback) {
+    const sql = "DELETE FROM measurements WHERE id=1";
+    db.run();
+}
+
+module.exports = { add, all, one, update, remove}
