e64db7172dd44790144b45542578badd34cc3029
anionce <anionce91@gmail.com>
2022-01-17 19:30:44 +0100

    finishing the bmi project

---
+++ backend/bmi-server/routes/stats.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+var Measurements = require("../models/measurements");
+
+router.get("/", function (req, res, next) {
+  Measurements.stats(function (err, data) {
+    if (err) {
+      res.status(500).send("Server Error");
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+module.exports = router;
