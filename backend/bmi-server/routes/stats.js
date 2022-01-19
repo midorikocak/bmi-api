98ff2b5f2b9b49fc0755186b4562b5ae7ef92aae
DestinyHime <arisubona@gmail.com>
2022-01-19 21:44:40 +0100

    MySQL DDBB added, status.hbs

--- backend/bmi-server/routes/stats.js
+++ backend/bmi-server/routes/stats.js
@@ -8,7 +8,27 @@ router.get("/", function (req, res, next) {
     if (err) {
       res.status(500).send("There was an error");
     } else {
-      res.json(data);
+      res.format({
+        html: function () {
+          res.render("stats", {
+            measurements: data.measurements,
+            averageBmi: data.averageBmi,
+          });
+        },
+        json: function () {
+          res.json(data);
+        },
+        text: function () {
+          res.send(
+            "Measurements: " +
+              data.measurements +
+              "Average BMI: " +
+              data.averageBmi
+          );
+        },
+      });
+
+      //res.json(data);
     }
   });
 });
